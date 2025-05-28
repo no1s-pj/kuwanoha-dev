0117bffad007239f5473f5998c782fcaec9be8f3
HASHIMOTO <hashimoto@no1s.biz>
2025-05-28 19:54:08 +0900

    first commit

---
+++ wget_bat.sh
@@ -0,0 +1,9 @@
+wget --mirror \
+    --convert-links \
+    --adjust-extension \
+    --page-requisites \
+    --span-hosts \
+    --domains=kuwanoha.com,cdn.shopify.com \
+    --no-parent \
+    https://kuwanoha.com/
+
