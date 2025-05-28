34316e0e0ab2b015982848c82ee9371bab480701
HASHIMOTO <hashimoto@no1s.biz>
2025-05-28 23:55:09 +0900

    ファイルネーム

--- .batch/rename.sh
+++ .batch/rename.sh
@@ -1,6 +1,8 @@
 # cd kuwanoha.com/s/files/1/0468/6669/4306/files/
 # cd kuwanoha.com/cdn/shop/products/
-cd kuwanoha.com/cdn/shop/articles/
+#cd kuwanoha.com/cdn/shop/articles/
+cd ../s/files/1/0468/6669/4306/products/
+
 for f in *\?v=*; do
   # 「?v=以下」を取り除いた部分を new に格納
   new="${f%%\?v=*}"
